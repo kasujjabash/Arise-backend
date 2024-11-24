813d3f34e8777085411044ff1d9eb2fcba2f63cc
kasujjabash <kasujjabash80@gmail.com>
2024-11-24 21:14:23 +0300

    Removed unused dependencies

--- app.py
+++ app.py
@@ -363,5 +363,5 @@ def get_videos():
     return jsonify(videos_data)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8080,debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
